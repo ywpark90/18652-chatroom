1415d94e2050dced0c7df29141c78e4dfd644cb6
Young Woong Park <ywpark90@gmail.com>
2015-01-16 00:34:34 -0500

    Basic chatroom model

--- fse_chatroom/chatroom/views.py
+++ fse_chatroom/chatroom/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from chatroom.models import *
 
-# Create your views here.
+def index(request):
+    chatlist = ChatRoom.objects.order_by('name')[:5]
+    context = {}
+
+    context['chatlist'] = chatlist
+
+    return render(request, 'chatroom/index.html', context)
+
+def chatroom(request, chatroom_id):
+    chat = get_object_or_404(ChatRoom, id=chatroom_id)
+    context = {}
+
+    context['chat'] = chat
+
+    return render(request, 'chatroom/chatroom.html', context)
